2c0afba846896eb250f7f791c81c00ca2cfa1eb8
André Secco <andreluizsecco@yahoo.com.br>
2017-07-20 22:55:56 -0300

    Novo exemplo do uso convencional do ref.

--- RefReturns/Program.cs
+++ RefReturns/Program.cs
@@ -13,11 +13,22 @@ namespace RefReturns
             Console.WriteLine($"Data Atual: {ObterDataAtual()}");
             Console.WriteLine(new string('-', 40));
 
+            //Convencional
+            AlterarDataAtual(ref _dataAtual);
+            Console.WriteLine($"Data Atual: {_dataAtual}");
+            Console.WriteLine(new string('-', 40));
+
+            //C# 7 - Ref Returns
             ref DateTime data = ref ObterDataAtualRefReturn();
             data = data.AddYears(20);
             Console.WriteLine($"Data Atual: {ObterDataAtual()}");
 
             Console.ReadKey();
+        }
+
+        private static void AlterarDataAtual(ref DateTime data)
+        {
+            data = data.AddYears(5);
         }
 
         private static DateTime ObterDataAtual()
